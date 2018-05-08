54c79894c103280d8a6d64f3079f8023b3410757
Fúlvio <fulviocanducci@hotmail.com>
2018-05-08 10:34:45 -0300

    Add project files.

---
+++ ClsAppEntityFrameworkCore21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClsAppEntityFrameworkCore21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
